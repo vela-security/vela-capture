827adf47d088b1bb3aad3e8903f0993a40a2aec6
edunx <jianguo@eastmoney.com>
2023-01-10 15:53:01 +0800

    新增模块

---
+++ lua.go
@@ -0,0 +1,67 @@
+package capture
+
+import (
+	"github.com/vela-security/vela-public/assert"
+	"github.com/vela-security/vela-public/export"
+	"github.com/vela-security/vela-public/lua"
+)
+
+var xEnv assert.Environment
+
+func newCaptureL(L *lua.LState) int {
+	cfg := newConfig(L)
+	proc := L.NewProc(cfg.name, typeOf)
+	if proc.IsNil() {
+		proc.Set(newCapture(cfg))
+	} else {
+		old := proc.Data.(*capture)
+		old.cfg = cfg
+	}
+	L.Push(proc)
+	return 1
+}
+
+func index(L *lua.LState, key string) lua.LValue {
+	switch key {
+	case "drop":
+		return lua.GoFuncErr(drop)
+	case "TCP":
+		return lua.LNumber(TCP)
+	case "UDP":
+		return lua.LNumber(UDP)
+	case "ICMP":
+		return lua.LNumber(ICMP)
+	case "DNS":
+		return lua.LNumber(DNS)
+	case "L4":
+		return lua.LNumber(uint64(TCP | UDP))
+	case "HAVE_IP":
+		return lua.S2L(DevHaveIP)
+	case "UP":
+		return lua.S2L(DevUp)
+	case "LOOPBACK":
+		return lua.S2L(DevLoopBack)
+	case "TCP_OUTBOUND_BPF":
+		return lua.S2L(TcpOutboundBPF())
+	case "UDP_OUTBOUND_BPF":
+		return lua.S2L(UdpOutboundBPF())
+	case "NOT_LOOPBACK":
+		return lua.S2L(DevNotLoopBack)
+	case "NOT_TCP_LISTEN_BPF":
+		return NotListenBPF("tcp")
+	case "NOT_UDP_LISTEN_BPF":
+		return NotListenBPF("udp")
+	case "NOT_TCP_LISTEN_PORT":
+		return NotListenPort("tcp")
+	case "NOT_UDP_LISTEN_PORT":
+		return NotListenPort("udp")
+	}
+
+	return lua.LNil
+}
+
+func WithEnv(env assert.Environment) {
+	xEnv = env
+	name := "vela.capture.export"
+	xEnv.Set("capture", export.New(name, export.WithIndex(index), export.WithFunc(newCaptureL)))
+}
